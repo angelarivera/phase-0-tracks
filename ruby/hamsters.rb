e9d2ee0e29d6ffcb51dd59b164b2e491c9c109ce
riveracangela@gmail.com <riveracangela@gmail.com>
2017-05-31 18:34:10 -0500

    Add hamster user info program

---
+++ ruby/hamsters.rb
@@ -0,0 +1,28 @@
+#hamsters name
+puts "Input name of hamster?"
+hamster_name = gets.chomp
+
+#hamsters volume level
+puts "What is the volume level?"
+volume_level = gets.chomp
+
+#hamsters fur color
+puts "What is the hamster color?"
+hamster_color = gets.chomp
+
+#hamsteradoption
+puts "Is the hamster a good candidate for adoption?"
+adoption = gets.chomp
+
+#hamsterage
+puts "What is the hamsters estimated age?"
+age = gets.chomp
+
+#if user leave age empty
+if age == ""
+	age = nil
+end
+
+
+
+
