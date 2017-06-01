286bd122863dc8f7759e8823a670a56798027b32
riveracangela@gmail.com <riveracangela@gmail.com>
2017-05-31 19:02:16 -0500

    add conditional statement and while loop

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -4,19 +4,35 @@ hamster_name = gets.chomp
 
 #hamsters volume level
 puts "What is the volume level?"
-volume_level = gets.chomp
+volume_level = gets.chomp.to_i
 
 #hamsters fur color
 puts "What is the hamster color?"
 hamster_color = gets.chomp
 
 #hamsteradoption
-puts "Is the hamster a good candidate for adoption?"
-adoption = gets.chomp
+puts "Is the hamster a good candidate for adoption? (y/n)"
+input=gets.chomp
+while input != "y" && input != "n"
+	puts "Not valid answer. Is the hamster a good candidate for adoption? (y/n)"
+	input=gets.chomp
+end
+if input == "y" 
+	adoption = true
+else 
+ 	 adoption = false	
+end
+
+
+# #hamsteradoption
+# puts "Is the hamster a good candidate for adoption? (y/n)"
+# adoption = gets.chomp
+
+
 
 #hamsterage
 puts "What is the hamsters estimated age?"
-age = gets.chomp
+age = gets.chomp.to_f
 
 #if user leave age empty
 if age == ""
