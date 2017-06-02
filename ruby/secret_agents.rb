3b6faf585076b75ea8d777766e69889e71a7c1fc
riveracangela@gmail.com <riveracangela@gmail.com>
2017-06-02 15:24:09 -0500

    Add encryption

---
+++ ruby/secret_agents.rb
@@ -0,0 +1,29 @@
+# Encrypt
+index = 0
+alpha = "ab c"
+
+while index < alpha.length
+	alpha[index] = alpha[index].delete(" ")
+	puts alpha[index] = alpha[index].next
+	index += 1
+end
+
+# Output below:
+# b
+# c
+# d
+
+# Decrypt
+
+# varaible = "abc"
+
+# z
+# a
+# b
+
+index = 0
+alpha = "abc"
+
+while index < alpha.length
+	puts alpha[0] = alpha[index].next
+
