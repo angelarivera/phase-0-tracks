078f2ea1735ce2e619918b431a93cc70cb6bb773
riveracangela@gmail.com <riveracangela@gmail.com>
2017-06-02 19:19:32 -0500

    Add correct variables

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,9 +1,9 @@
-# *! Does the until loop need to have the number of employees assigned to the index (for the countdown)? Does the bottom half of the while loop belong at the end of the program? Why doesn't the subtraction portion of the employee number work?
 puts "How many employees will be processed?"
 number_of_employees = gets.chomp.to_i
 
-i = number_of_employees
-until i == 0 
+index = 0
+while index < number_of_employees
+
 # Employee name
 puts "What is your name?"
 employee_name = gets.chomp
@@ -37,32 +37,32 @@ employee_insurance_choice = gets.chomp
 	end 
 
 # Employee allergies & while loop
-puts "Name any allergies, one at a time. Please type "done" when you are finished."
+puts "Name any allergies, one at a time. Please type 'done' when you are finished."
 employee_allergies = gets.chomp
 
 # *! Is the while loop written concisely?
-i = employee_allergies
-while i != "done" || "sunshine"
+index = employee_allergies
+while index != "done" || index !="sunshine"
 	puts "What other allergies do you have?"
-	if i == "sunshine"
+	break if index == "done" || index == "sunshine"
+		if index == "sunshine"
 		# !* What does DBC mean when they say skip directly to the result of "probably a vampire? Do they just want me to print that result? Do I have to insert that within the case statement? Which means I need to create valid and invalid allergies variables?""
 		puts "Probably a vampire."
-	break if i == "done" || "sunshine"
 end
-
+end
 
 # Set up variables for conditional logic
 # Establish correct age
-# *! How do I get year in ruby? Is the rest correct?
+# *! Year in ruby? Is the rest correct?
 # *! Do I need an invalid age?
-time = Time.new
-valid_age = time - employee_birth_year == employee_age
+current_year = Time.new
+valid_age = current_year - employee_birth_year == employee_age
 invalid_age = !valid_age
 
 # Establish correct garlic choice
 # *! Am I establishing the valid and invalid choices correctly by "y" and "n"? Same for insurance
-valid_garlice_choice = employee_insurance_choice == "y"
-invalid_garlic_choice = employee_insurance_choice == "n"
+valid_garlic_choice = employee_garlic_choice == "y"
+invalid_garlic_choice = employee_garlic_choice == "n"
 
 # Establish correct insurance choice
 valid_insurance_choice = employee_insurance_choice == "y" 
@@ -70,34 +70,40 @@ invald_insurance_choice = employee_insurance_choice == "n"
 
 
 # Detection Logic
-# *! What do I put after the case. I know it has to be an object, but in this instance, is there a designated object? Or is just something I can name? 
-# *! I put down result before the "= case" because I saw it formatted that way online, is that correct?
+puts results = "Resutls inconclusive"
 
-employee = employee_name
+# If the employee got their age right, and is willing to eat garlic bread or sign up for insurance, the result is “Probably not a vampire.”
+if valid_age && valid_garlice_choice || valid_insurance_choice
+	results = "Probably not a vampire"
+end
 
-result  = case employee
-when valid_age && valid_garlice_choice || valid_insurance_choice
-	puts "Probably not a vampire"
-when
+# If the employee got their age wrong, and hates garlic bread or waives insurance, the result is “Probably a vampire.”
+if
 	invalid_age && invalid_garlic_choice || valid_insurance_choice
-	puts "Probably a vampire"
-when
+	results = "Probably a vampire"
+end
+
+# If the employee got their age wrong, hates garlic bread, and doesn’t want insurance, the result is “Almost certainly a vampire.”
+if
 	# *!If the employee got their age wrong, hates garlic bread, and doesn’t want insurance. Does the fact that there is no "and" that separates the employee age and garlic bread change anything?
 	invalid_age && invalid_garlic_choice && invalid_age
-	puts "Almost certainly a vampire"
-when 
-	employee_name == "Drake Cula" || "Tu Fang"
-	puts "Definitely a vampire"
-else 
-	puts "Resutls inconclusive"
+	results = "Almost certainly a vampire"
+end
+
+#Even if the employee is an amazing liar otherwise, anyone going by the name of “Drake Cula” or “Tu Fang” is clearly a vampire, because come on. In that case, you should print “Definitely a vampire.”
+if 
+	employee_name == "Drake Cula" || employee_name == "Tu Fang"
+	results = "Definitely a vampire"
 end
 
 # Print survey results
-# !*Is this how I call the case statement to execute the above?
-puts case "The employee is #{employee}."
+# !* Now that I've switched to if statements, do I remove the case and just write puts results?
+puts results
 
-# Plot twist does this need to be embedded within the initial while loop from the start?
+# # Plot twist
+# if index #is about to finish
+# 	puts "Actually, never mind! What do these questions have to do with anything? Let's all be friends."
+
+index += 1
 puts "Actually, never mind! What do these questions have to do with anything? Let's all be friends."
-
-i -= 1 
-end 
+end
