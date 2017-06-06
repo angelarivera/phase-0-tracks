27711d8b19220659f58fa0b38f4edcd8015f8e4a
riveracangela@gmail.com <riveracangela@gmail.com>
2017-06-05 22:05:27 -0500

    Add array and hash

---
+++ ruby/iteration.rb
@@ -0,0 +1,12 @@
+# Declare an array
+bay_area_neighborhoods = [Lake Merritt, Grand Lake, Downtown Oakland, Mission District]
+
+# Declare a hash
+
+neighborhood_needs = {
+	Public Transporation: "",
+	Safety Level: "",
+	Night Life Level: "", 
+	Grocery Stores: "",
+	Gyms: ""
+}
