3f873cef488e2e6a781a7ac4d03b7ff76b3e412f
riveracangela@gmail.com <riveracangela@gmail.com>
2017-06-06 22:21:19 -0500

    Add pseudocode

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,59 @@
+# Write a program that takes a real name and creates a fake name
+
+# Puts Ask for first name
+# first_name = gets.chomp
+
+# Ask for last name
+
+# Select first name, convert the string to lowercase letters
+# Select lower case first name, convert the string to an array
+
+
+# Iterate through the array 
+
+# def vowelchanger (full name)
+# vowels = [a,e,i,o,u]
+# for each letter in the name
+# see if its a vowel
+# if it is then what is the index of the vowel
+# and set that vowel to the following +1 item index 
+# if the letter in the full namer variable is "u" 
+# then program the computer to automatically go to the letter a 
+# return value will be changed name (with vowels changed)
+# set that return value to a new variable 
+
+
+# def consonantchanger (full name)
+# get result from new name from vowel changer
+# consants = [b,c,f,l]
+# return value will be changed name 
+
+# new first name
+
+
+#    If an item in the first name array has the letters "a,e,i,o, u" take the index that vowel is in and change it to the following vowel. (Example: a -> e) 
+#    Create an alphabet variable that has "a-z" to reference.
+#    If an item in the first name array has a consonant take the index of that consonant and change it to the letter that follows it in the alphabet
+# Place both of these changes in an empty array (.map)? Or .map!?
+
+
+
+
+
+
+
+
+
+
+
+
+# Select last name, convert the string to lowercase letters
+# Select lower case last name, convert the string to an array
+
+# Iterate through the array, 
+#    If an item in the last name array has the letters "a,e,i,o, u" take the index that vowel is in and change it to the following vowel. (Example: a -> e) 
+#    Create an alphabet variable that has "a-z" to reference.
+#    If an item in the last name array has a consonant take the index of that consonant and change it to the letter that follows it in the alphabet
+# Place both of these changes in an empty array (.map)? Or .map!?
+
+# one method per step, insdie that method you can be focused on that one thing you have to do! 
