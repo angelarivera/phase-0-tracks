f23605627cf4791bcabbd960fba91c4406f29ce6
riveracangela@gmail.com <riveracangela@gmail.com>
2017-06-08 16:27:39 -0500

    Add vowels method

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,18 +1,38 @@
 # Write a program that takes a real name and creates a fake name
 
-# Puts Ask for first name
-# first_name = gets.chomp
+# Ask for first name
+puts "What is your first name?"
+first_name = gets.chomp
 
 # Ask for last name
+puts "What is your last name?"
+last_name = gets.chomp
+
+# add first and last name into string
+
 
 # Select first name, convert the string to lowercase letters
-# Select lower case first name, convert the string to an array
+user_name.downcase 
 
+# Select lower case first name, convert the string to an array
+user_name.split("")
+
+# Put items in array
+full_name = user_name.split
 
 # Iterate through the array 
+def name_changer(user_name)
+	vowels = ["a", "e", "i", "o", "u", "y"]
+	user_name.each  do |letter|
+		if vowels.include letter
+		index[letter] == .next
 
-# def vowelchanger (full name)
-# vowels = [a,e,i,o,u]
+
+	# consonants
+
+
+end  
+
 # for each letter in the name
 # see if its a vowel
 # if it is then what is the index of the vowel
