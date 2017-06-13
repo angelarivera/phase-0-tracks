802d20a7b24f1dd0f620d992049319abccafc593
riveracangela@gmail.com <riveracangela@gmail.com>
2017-06-12 19:54:04 -0500

    Design puppy class

---
+++ ruby/puppy.rb
@@ -0,0 +1,16 @@
+# Species -----------------------------------------
+# Canis (Canines)
+
+# Characteristics ---------------------------------
+# Sense of Smell: Strong
+# Sensitivity to Earths Magnetic Field: Strong
+# Coats: Single or Double
+# Lifespan: Varies
+# Size: Varies
+# Color: Varies
+
+# Behavior -----------------------------------------
+# Run
+# Hunt 
+# Jump
+# Bark
