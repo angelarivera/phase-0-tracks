c41a1ae1ca8c636f3afe055d984f6f20d7808716
riveracangela@gmail.com <riveracangela@gmail.com>
2017-06-13 01:06:32 -0500

    Add reindeer array

---
+++ ruby/santa.rb
@@ -0,0 +1,22 @@
+# R E L E A S E   Z E R O 
+
+class Santa 
+	def initialize
+		@gender = ["agender", "female", "bigender", "gender fluid"]
+		@ethnicity = ""
+	end
+
+	def speak
+		puts "Ho, ho, ho! Haaaappy holidays!"
+	end
+
+	def eat_milk_and_cookies (cookietype)
+		puts "That was a good #{cookietype}!"
+	end
+end 
+
+# R E L E A S E   O N E 
+
+reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+
+age = 0 
