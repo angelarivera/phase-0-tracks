1fa3ce055f10e9f71f32369d35361c225ac4d4e0
riveracangela@gmail.com <riveracangela@gmail.com>
2017-06-13 22:21:12 -0500

    Refactored code

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -7,17 +7,18 @@
   # Set default quantity to 1 and assign that to each that you iterate through
   # print the list to the console [final method listed]
 # output: [hash]
+# { |thing_to_print| puts thing_to_print }
 
+
+# Refactored line 16! 
 def create_list (items)
 	grocery_list = {}
-	items.split(" ").each do |item|
-		grocery_list[item] = 1
-	end
+	items.split(" ").each { |item| grocery_list[item] = 1}
 	puts grocery_list
 	return grocery_list
 end
 
-my_list = create_list("carrots apples cereal pizza")
+# my_list = create_list("carrots apples cereal pizza")
 
 
 # Method to add an item to a list
@@ -30,8 +31,8 @@ def add_item (grocery_list, item, quantity)
 	return grocery_list
 end
 
-my_updated_list = add_item(my_list, "water", 2)
-puts my_updated_list
+# my_updated_list = add_item(my_list, "water", 2)
+# puts my_updated_list
 
 # Method to remove an item from the list
 # input: item name, list
@@ -43,8 +44,8 @@ def remove_item (my_updated_list, item)
 	return my_updated_list
 end
 
-my_updated_list = remove_item(my_updated_list, "pizza")
-puts my_updated_list
+# my_updated_list = remove_item(my_updated_list, "pizza")
+# puts my_updated_list
 
 
 # Method to update the quantity of an item
@@ -57,24 +58,52 @@ def update_quantity (grocery_list, item, quantity)
 	return grocery_list
 end
 
-my_updated_list = update_quantity(my_updated_list, "apples", 3)
-puts my_updated_list
+# my_updated_list = update_quantity(my_updated_list, "apples", 3)
+# puts my_updated_list
 
 
 # Method to print a list and make it look pretty
 # input: grocery_list
 # steps: iterate throught grocery_list and print each item name and quantity. Add tittle that says "My Grocery List" and name
 # output: Pretty list
+# { |thing_to_print| puts thing_to_print }
 
+
+# Refactored line 26!
 def print_list (grocery_list)
 	puts "My Grocery List"
 	puts "Item              Quantity"
-	grocery_list.each do |item, quantity|
-		puts "#{item}              #{quantity}"
-	end
+	grocery_list.each {|item, quantity| puts "#{item}              #{quantity}"} 
 end
+#
+# print_list(my_updated_list)
 
-print_list(my_updated_list)
+
+
+# D R I V E R   C O D E 
+
+new_list = create_list("")
+add_item(new_list, "Lemonade", 2)
+add_item(new_list, "Tomatoes", 3)
+add_item(new_list, "Onions", 1)
+add_item(new_list, "Ice Cream", 4)
+print new_list
+
+remove_item(new_list, "Lemonade")
+print new_list
+
+update_quantity(new_list, "Ice Cream", 1)
+print new_list
+
+print_list(new_list)
+
+
+
+
+
+
+
+
 
 
 
