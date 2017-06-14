91a4f7822f191a9a567dd3c1fde347e62abcff46
riveracangela@gmail.com <riveracangela@gmail.com>
2017-06-13 21:04:49 -0500

    Initialized method

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,9 +1,14 @@
 # R E L E A S E   Z E R O 
 
 class Santa 
-	def initialize
-		@gender = ["agender", "female", "bigender", "gender fluid"]
-		@ethnicity = ""
+	def initialize (gender, ethnicity)
+		# Can't have a Santa with no gender or ethnicty.
+		# The following are instance variables.
+		# The @ sign lets it be used throughout the entire class. 
+		@gender = gender
+		@ethnicity = ethnicity
+		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+		@age = 0 
 	end
 
 	def speak
@@ -15,8 +20,30 @@ class Santa
 	end
 end 
 
+
+
 # R E L E A S E   O N E 
 
-reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+# Declaring an empty array to store diverse santas
+santas = []
 
-age = 0 
+# Setting example_genders array for loop to reference
+gender_options = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+
+# Setting example_ethnicities array for loop to reference
+ethnicity_options = ["African American", "Latinx", "Caucasian", "Asian", "Native American", "prefer not to say"]
+
+# creating a do loop to iterate through the array and initialize diverse santas into the empty Santas array. <---- Don't understand index
+gender_options.length.times do |index|
+	santas << Santa.new(gender_options[index], ethnicity_options[index])
+end
+
+
+
+
+# R E L E A S E   T W O 
+
+
+
+
+
