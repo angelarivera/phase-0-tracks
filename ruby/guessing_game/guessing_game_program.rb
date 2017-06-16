f6cee1d2cf5ca8cdcbdd0834f6879475b88ec47f
riveracangela@gmail.com <riveracangela@gmail.com>
2017-06-16 14:38:40 -0500

    Add while loop, restructure psuedocode

--- ruby/guessing_game/guessing_game_program.rb
+++ ruby/guessing_game/guessing_game_program.rb
@@ -5,16 +5,75 @@
 # The user should get a congratulatory message if they win, and a taunting message if they lose.
 
 class Game
-	def initialize 
+	attr_reader :guess_count
+	attr_accessor :secret_word, :letter_guess
+
+	def initialize (secret_word)
+		@secret_word = secret_word
+		@letter_guess = letter_guess
+		@guess_count = 0
+		# Takes word and splits the string into items in an array
+		@secret_word_array = secret_word.split
+		@guessed_letters = []
+		# Array legnth determined by length of secret word.
+		@max_guess = secret_word.length
 	end
 
+	def take_in_guess
+		# input of a letter 
+		# checks the guess and decides what to update
+	end
 
+	def game_status
+		# figure out a way to retrive/calculate what letters have been guessed.
+		secret_word.length
+		game_status = []
+		# secret_word.length
+		# secret_word.each do |letter|
+			# read each letter with an "_"
+			# decide whether to put in a letter or underscore
+		# game_status << secret_word.length.times
+	end 
+
+	# Creates an empty array to store guesses
+	guessed_letters = []
+
+	# Defines the max number of guessses allowed
+	max_guess = secret_word.length
+
+	def guess_counter(index)
+		if @letter_guess != secret_word_array
+			@guess_count += 1
+		end 
+	end
+
+	def win_message
+		if game_status == secret_word_array
+			puts "You win!"
+		end
+	end 
+	# create a method that takes in letter guess
 end
 
 # D R I V E R   C O D E
-puts "What word do you want the user to guess?"
-secret_word = gets.chomp
 
+loop do 
+	if guess_counter < max_guess
+	puts "Welcome to the guessing game"
+	# Create a new_game instance of the game class
+	new_game = Game.new 
+
+	puts "Player one, what word do you want the user to guess?"
+	secret_word = gets.chomp
+
+	puts "Player two, what letter do you want to guess?"
+	puts new_game.game_status # => "_ _ _ _ _" (length determind by secret word)
+	letter_guess = gets.chomp
+
+	else 
+	puts "Game Over!"
+end 
+end 
 
 
 
@@ -29,6 +88,7 @@ secret_word = gets.chomp
 # Set a limit of user guesses based off the length of the word
 # While (guesses is less than max guesses) user hasn't reached guess count
 	# Display to user two the hidden word they need to guess "_ _ _ _ _" 
+	# How do you generate that hidden word...
 	# Ask user two to guess a letter (z)
 	# Assign guessed letter a variable
 	# If guessed letter == a letter in the word array
@@ -37,7 +97,7 @@ secret_word = gets.chomp
 		# Once I find that index, I want to replace the index with the letter
 		# Replace the letter with the index
 		# Puts SECOND VARIABLE with correct letter they guessed "_ _ z z _"
-		# And update the second variable with the correct letter guessed "_ _ z z _"
+		# And update the game_status with the correct letter guessed "_ _ z z _"
 		# And update the displayed word the user sees
 	# Else add to the guess count
 	# Put "incorrect guess, guess again"
@@ -58,4 +118,3 @@ secret_word = gets.chomp
 # Elsif the guess is correct, do not count towards the guess count
 # If user two reaches the end of the guess count
 	# Puts "Sorry you lost!"
-
