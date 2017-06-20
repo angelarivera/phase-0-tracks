88ad12bfb9c2575c5045cab926b6ca6ff87247fb
riveracangela@gmail.com <riveracangela@gmail.com>
2017-06-20 12:24:14 -0500

    Add methods to TodoList class

--- ruby/list/todo_list.rb
+++ ruby/list/todo_list.rb
@@ -1,25 +1,49 @@
-class TodoList
-	attr_accessor :tasks
-
-	def initialize (params = [])
-		@tasks = params.push
+class TodoList 
+	def initialize(array)
+		@list = array
 	end
 
 	def get_items
-		@tasks 
+		@list 
+	end 
+
+	def add_item(item)
+		@list << item
 	end
 
-	def add_item(task)
-		@tasks << task
-		@tasks
-	end
-
-	def delete_item(task)
-		@tasks.delete(task)
-		@tasks
+	def delete_item(item)
+		@list.delete(item)
 	end
 
 	def get_item(index)
-		@tasks[index]
-	end 
+		@list[index]
+	end
 end
+
+
+
+# class TodoList
+# # 	attr_accessor :tasks
+
+# # 	def initialize (params = [])
+# # 		@tasks = params.push
+# # 	end
+
+# # 	def get_items
+# # 		@tasks 
+# # 	end
+
+# # 	def add_item(task)
+# # 		@tasks << task
+# # 		@tasks
+# # 	end
+
+# # 	def delete_item(task)
+# # 		@tasks.delete(task)
+# # 		@tasks
+# # 	end
+
+# # 	def get_item(index)
+# # 		@tasks[index]
+# # 	end 
+# end
