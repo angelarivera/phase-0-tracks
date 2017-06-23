9915f52427308268bc952c822d0997b431b5a133
riveracangela@gmail.com <riveracangela@gmail.com>
2017-06-22 22:47:01 -0500

    Add for for loop and conditional

--- js/algos.js
+++ js/algos.js
@@ -12,15 +12,12 @@ const wordSorter = function(words) {
 
 const sortedArray = wordSorter(wordsArr);
 
+console.log(sortedArray);
+
 
 /* R E L E A S E   O N E 
 Write a function that takes two objects and checks to see if the objects share at least one key-value pair. Will return either true or false. 
 */
-
-var foo = { 'alpha' : 'puffin', 'beta' : 'beagle' };
-var keys = Object.keys(foo); // ['alpha', 'beta'] 
-// (or maybe some other order, keys are unordered).
-
 
 var objectOne = {
 	name: "Steven",
@@ -28,26 +25,32 @@ var objectOne = {
 }
 
 var objectTwo = {
-	name: "Julia",
-	age: 40
+	name: "Steven",
+	race: "Latino"
 }
 
 const checkMatch = function(objectOne, objectTwo) {
-	console.log(keys = Object.keys(objectOne));
-	console.log(values = keys.map(function(v){return objectOne[v];}))
-	console.log(keys2 = Object.keys(objectTwo));
-	console.log(values2 = keys.map(function(v){return objectTwo[v];}))
+	// console.log(keys = Object.keys(objectOne));
+	// console.log(values = keys.map(function(v){return objectOne[v];}))
+	// console.log(keys2 = Object.keys(objectTwo));
+	// console.log(values2 = keys.map(function(v){return objectTwo[v];}))
+
+	for (key in objectOne) {
+		if (objectTwo.hasOwnProperty(key)){
+			console.log(key in objectTwo);
+		}
+	}
 	// console.log(values = Object.values(objectOne));
 }
 
 console.log(checkMatch(objectOne, objectTwo));
 
+/* R E L E A S E   T W O 
+Write a function that takes an integer for length, and builds and returns an array of strings of the given length. So if we ran your function with an argument of 3, we would get an array of 3 random words back (the words don't have to be actual sensical English words -- "nnnnfph" totally counts). The words should be of randomly varying length, with a minimum of 1 letter and a maximum of 10 letters. (This involves a new trick, generating a random number, that you'll have to look up, but the solution on how to do so is relatively straightforward.)
 
-/* D R I V E R   C O D E 
+Add driver code that does the following 10 times: generates an array, prints the array, feeds the array to your "longest word" function, and prints the result.
+*/ 
 
-For Release One
-console.log(sortedArray);
-*/
 
 
 
