0d42bc7e4f1bc5c098b017ba491c599a79e25e34
riveracangela@gmail.com <riveracangela@gmail.com>
2017-06-29 19:32:24 -0500

    deleted files

--- databases/Shopping_List/shoppinglist.rb
+++
@@ -1,7 +0,0 @@
-# require gems
-require 'sqlite3'
-require 'faker'
-
-# create SQLite3 database
-# create a new database and store it in db variable.
-$DATABASE = SQLite3::Database.new("shoppinglist.db")
