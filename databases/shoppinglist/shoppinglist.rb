63a1e8950c6e10f8167a7637cd7b40d596d5b9aa
riveracangela@gmail.com <riveracangela@gmail.com>
2017-06-29 19:48:19 -0500

    Add gems, create shopping list database

---
+++ databases/shoppinglist/shoppinglist.rb
@@ -0,0 +1,13 @@
+# SHOPPING LIST 
+
+# require gems
+require 'sqlite3'
+require 'faker'
+
+# create SQLite3 database, store in a global variable
+$DATABASE = SQLite3::Database.new("shoppinglist.db")
+
+
+
+
+
