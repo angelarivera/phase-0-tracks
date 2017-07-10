cfb407688890c37963d20d22b65922973d600c0d
riveracangela@gmail.com <riveracangela@gmail.com>
2017-07-09 22:58:22 -0500

    Add and execute string delimiters

--- databases/shoppinglist/pitchfork.rb
+++ databases/shoppinglist/pitchfork.rb
@@ -2,5 +2,26 @@
 require 'sqlite3'
 require 'faker'
 
-# create SQLite3 database
+# create Pitchfork database
 $DATABASE = SQLite3::Database.new("pitchfork.db")
+
+# create user and performer table w/ string delimiters
+create_user_table = <<-SQL
+	CREATE TABLE IF NOT EXISTS user(
+		users_id INTEGER PRIMARY KEY,
+		name VARCHAR(255),
+		performer_id INT,
+		FOREIGN KEY (performer_id) REFERENCES performer(performers_id) 
+	)
+SQL
+
+create_performer_table = <<-SQL
+	CREATE TABLE IF NOT EXISTS performer(
+		performers_id INTEGER PRIMARY KEY, 
+		band_name VARCHAR(255)
+	)
+SQL
+
+# execute creation of tables
+$DATABASE.execute(create_user_table)
+$DATABASE.execute(create_performer_table)
